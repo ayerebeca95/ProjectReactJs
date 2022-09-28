6860428b55aa12f3c66d56bc8ffb4d9e6077271b
ayerebeca95 <ayereebeca95@gmail.com>
2022-09-28 16:00:03 -0400

    agregando detalles en firebase

--- newapp/src/components/ItemDetailContainer/index.jsx
+++ newapp/src/components/ItemDetailContainer/index.jsx
@@ -2,12 +2,7 @@ import React, { useEffect, useState} from 'react';
 import { getFirestore, doc, getDoc } from "firebase/firestore";
 import ItemDetail from "../ItemDetail";
 import { useParams } from "react-router-dom";
-//import piscina1  from "../../assets/piscina1.jpg";
-//import piscina2  from "../../assets/piscina2.jpg";
-//import piscina3  from "../../assets/piscina3.jpg";
-//import piscina4  from "../../assets/piscina4.jpg";
-//import jacuzzi1  from "../../assets/jacuzzi1.jpg";
-//import jacuzzi2  from "../../assets/jacuzzi2.jpg";
+
 
 
 export const ItemDetailContainer = () => {
